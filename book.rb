ceb29925cb2800a8a1a25a2d995ce0e1cd550cfb
Matt Buck <github@matthewbuck.com>
2013-09-19 16:12:51 -0500

    Add Book#overdue method.

--- book.rb
+++ book.rb
@@ -1,10 +1,15 @@
 class Book
   attr_reader :title, :author
-  attr_accessor :due_date, :user
+  attr_accessor :due_date
 
   def initialize(title, author, description = nil)
     @title       = title
     @author      = author
     @description = description
+  end
+
+  # Returns true if a book is overdue, false otherwise.
+  def overdue?
+    Time.now > @due_date
   end
 end
