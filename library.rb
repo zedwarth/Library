376b425677f0333adb4f4fcfba4eead49d0b8a84
Michael Ornellas <mike@themakersquare.com>
2013-11-15 13:09:52 -0600

    Revert "added some code in for class to work from"
    This reverts commit ff33977d12c7180394ae5ef72c1d343bae49091a.

--- library.rb
+++ library.rb
@@ -16,20 +16,9 @@ class Library
   end
 
   def add_book(book)
-    @books << book
   end
 
   def check_out(user, book)
-    if user.borrowed_books.length == 2
-      return "Sorry, that user already has two books checked out"
-    end
-
-    if book.status == "available"
-      book.borrower = user
-      book.status = "checked out"
-    else
-      return "Sorry, that book is not available"
-    end
   end
 
   def check_in(book)
@@ -38,7 +27,6 @@ end
 
 class Borrower
   def initialize(name)
-    @name = name
   end
 
   def borrowed_books
@@ -55,39 +43,6 @@ class Borrower
 end
 
 class Book
-  # attr_reader :title, :author
   def initialize(title, author)
-    @title = title
-    @author = author
-    @status = "available"
-    @borrower = nil
-  end
-
-  def title
-    @title
-  end
-
-  def author
-    @author
-  end
-
-  def status
-    @status
-  end
-
-  def status=(new_value)
-    @status = new_value
-  end
-
-  def borrower
-    @borrower
-  end
-
-  def borrower=(new_value)
-    @borrower = new_value
   end
 end
-
-borrower = Borrower.new
-
-book = Book.new("Jurassic Park", "Michael Crichton")
