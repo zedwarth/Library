13617d6ba8834f81cd144e9e491fb10c86f5d068
Gilbert <gilbertbgarza@gmail.com>
2014-02-07 11:03:58 -0600

    Upgrade to rspec

--- library.rb
+++ library.rb
@@ -1,3 +1,14 @@
+
+class Book
+  def initialize(title, author)
+  end
+end
+
+class Borrower
+  def initialize(name)
+  end
+end
+
 class Library
   def initialize(name)
     @books = []
@@ -6,43 +17,18 @@ class Library
   def books
   end
 
-  def list_books
+  def add_book(title, author)
   end
 
-  def borrowed_books
+  def check_out_book(book_id, borrower)
+  end
+
+  def check_in_book(book)
   end
 
   def available_books
   end
 
-  def add_book(book)
-  end
-
-  def check_out(user, book)
-  end
-
-  def check_in(book)
-  end
-end
-
-class Borrower
-  def initialize(name)
-  end
-
   def borrowed_books
-  end
-
-  def name
-  end
-
-  def borrowed_books_count
-  end
-
-  def borrowed_books_list
-  end
-end
-
-class Book
-  def initialize(title, author)
   end
 end
