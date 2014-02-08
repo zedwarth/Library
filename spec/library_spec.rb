42595c4065d1695687a810ab8b7fc116d8d3e1d6
zedwarth <zedwarth@gmail.com>
2014-02-07 18:28:23 -0600

    Rewrite invalid class names.

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -39,15 +39,15 @@ describe Book do
     expect(book.status).to eq 'available'
   end
 
-	it "has optional year-published and edition parameters" do
+	it "has optional year_published and edition parameters" do
 		# If they aren't set, should return nil
     book = Book.new("The Stranger", "Albert Camus")
-		expect(book.year-published).to be_nil
+		expect(book.year_published).to be_nil
 		expect(book.edition).to be_nil
 
 		# If they are set, return the date or edition
     book = Book.new("The Stranger", "Albert Camus", "1943", "First")
-		expect(book.year-published).to eq "1943"
+		expect(book.year_published).to eq "1943"
 		expect(book.edition).to eq "First"
 	end
 end
